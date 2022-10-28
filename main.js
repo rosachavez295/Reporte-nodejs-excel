e165f84aaca6477c1e6946f3f50c59c6bb04787b
rosachavez295 <kimrosita56@gmail.com>
2022-10-27 20:28:46 -0400

    dd

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ const sequelize = new Sequelize("Prueba", "username", "password", {
   host: "localhost",
   dialect: "mssql",
 });
-.
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
